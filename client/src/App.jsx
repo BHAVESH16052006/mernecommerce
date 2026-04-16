f6efbaa957fb5bae0c26e24ebd8bbf06de9d0309
BHAVESH16052006 <bhaveshpenneru18@gmail.com>
2026-04-16 16:19:17 +0530

    day2 admin,add product

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Register from './components/Register'
-
+import Login from './components/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+import AddProduct from './components/Addproduct'
 export default function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element/>
-        <Route path='/register' element={<Register/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/add-product' element={
+          <ProtectedRoute>
+            <AddProduct />
+          </ProtectedRoute>} />
+
       </Routes>
     </BrowserRouter>
   )
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,22 +1,28 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import API from '../api/axios'
 export default function Register() {
-    const [name,setName]=useState("")
-    const [email,setEmail]=useState("")
-    const [password,setPassword]=useState("")
-    const [mobile,setMobile]=useState("")
-    function handleRegister(e){
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [mobile, setMobile] = useState("")
+    function handleRegister(e) {
         e.preventDefault()
-       
-        API.post("/auth/register",{name,email,password,mobile:Number(mobile)})
-            .then((res)=>{
-                console.log(res)
-                if(res.status===201){
+
+        API.post("/auth/register", { name, email, password, mobile: Number(mobile) })
+            .then((res) => {
+                console.log("from then method", res)
+                if (res.status === 201) {
                     alert("Registration successfully")
                 }
+                else if (res.status == 401) {
+                    alert(res.data.message)
+                }
             })
-            .catch(err=>{
-                console.log(err)
+            .catch(err => {
+                console.log("from catch block", err)
+                if (err.status == 401) {
+                    alert(err.response.data.message)
+                }
             })
     }
     return (
@@ -28,35 +34,35 @@ export default function Register() {
                     </div>
                     <div className="mb-3">
                         <label htmlFor="" className="form-label">Name</label>
-                        <input 
-                            type="text" 
-                            className="form-control" 
+                        <input
+                            type="text"
+                            className="form-control"
                             name="name"
-                            onChange={(e)=>setName(e.target.value)}/>
+                            onChange={(e) => setName(e.target.value)} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="" className="form-label">Email</label>
-                        <input 
-                            type="email" 
-                            className="form-control" 
+                        <input
+                            type="email"
+                            className="form-control"
                             name="email"
-                            onChange={(e)=>setEmail(e.target.value)}/>
+                            onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="" className="form-label">Password</label>
-                        <input 
-                            type="password" 
-                            className="form-control" 
+                        <input
+                            type="password"
+                            className="form-control"
                             name="password"
-                            onChange={(e)=>setPassword(e.target.value)}/>
+                            onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="" className="form-label">Mobile Number</label>
-                        <input 
-                            type="text" 
-                            className="form-control" 
+                        <input
+                            type="text"
+                            className="form-control"
                             name="mobile"
-                            onChange={(e)=>setMobile(e.target.value)}/>
+                            onChange={(e) => setMobile(e.target.value)} />
                     </div>
                     <button className='btn btn-success'>Register</button>
                 </form>
